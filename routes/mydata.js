262e1e969c54aecb23ea40346255596d2765b3d5
Kurri <S566991@nwmissouri.edu>
2023-10-25 12:19:36 -0500

    updated mydata

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mahitha sri kurri' });
+  res.render('mydata', { title: 'Mahitha sri kurri' });
 });
 
 module.exports = router;
