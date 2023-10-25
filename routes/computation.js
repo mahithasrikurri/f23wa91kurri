c9988e5a19a74a2dac4a175410f8653d858e3b3f
Kurri <S566991@nwmissouri.edu>
2023-10-25 12:44:12 -0500

    updated computation

--- routes/computation.js
+++ routes/computation.js
@@ -1,15 +1,19 @@
-var express = require('express');
-var router = express.Router();
 
-router.get('/', function(req, res, next) {
- if(req.query.x==undefined) {
-    value = Math.random();
- } 
- else {
-    value = req.query.x;
- }
- var result = Math.log10(value);
-  res.render('computation', { function: `Math.log10(${value}) is ${result}` });
-});
+  res.render('computation', { function: `Math.abs(${value}) is ${result}` });
 
-module.exports = router;
+
+  var express = require('express');
+  var router = express.Router();
+   
+  router.get('/', function(req, res, next) {
+   if(req.query.x==undefined) {
+      value = Math.random();
+   }
+   else {
+      value = req.query.x;
+   }
+   var result = Math.round(value);
+    res.render('computation', { com: `Math.abs(${value}) is ${result}` });
+  });
+   
+  module.exports = router;
